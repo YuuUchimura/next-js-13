daef6804de63b1133e46a95f93f304a2bb952828
Yuu <anpanmamZ3624@gmail.com>
2023-08-20 18:06:56 +0900

    Route Handlers

--- src/app/users/UserList.tsx
+++ src/app/users/UserList.tsx
@@ -5,10 +5,13 @@ type User = {
 };
 
 const UserList = async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  // await new Promise((resolve) => setTimeout(resolve, 5000));
+  const response = await fetch("http://localhost:3000/api?name=John", {
+    cache: "no-store",
+  });
   if (!response.ok) throw new Error("Failed to fetch data");
   const users: User[] = await response.json();
-  console.log(users);
+
   return (
     <ul>
       {users.map((user) => (
