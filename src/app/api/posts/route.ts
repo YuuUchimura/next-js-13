c561be208ff134b34d7e6e4882d948e8b32996a2
Yuu <anpanmamZ3624@gmail.com>
2023-08-24 08:46:31 +0900

    build

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -2,13 +2,11 @@ import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
 export async function GET() {
-  console.log("Get!");
   const posts = await prisma.post.findMany();
   return NextResponse.json(posts);
 }
 
 export async function POST(request: Request) {
-  console.log("Post!");
   const req = await request.json();
   await prisma.post.create({ data: req });
 
--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -2,19 +2,19 @@ import { Post } from "@prisma/client";
 import AddPost from "./AddPost";
 
 const Page = async () => {
-  const response = await fetch("http://localhost:3000/api/posts", {
-    method: "GET",
-    cache: "no-store",
-  });
-  const posts: Post[] = await response.json();
+  // const response = await fetch("http://localhost:3000/api/posts", {
+  //   method: "GET",
+  //   cache: "no-store",
+  // });
+  // const posts: Post[] = await response.json();
 
   return (
     <div className="m-4">
       <h1 className="text-lg font-bold">記事一覧</h1>
       <ul>
-        {posts.map((post) => (
+        {/* {posts.map((post) => (
           <li key={post.id}>{post.title}</li>
-        ))}
+        ))} */}
       </ul>
       <AddPost />
     </div>
